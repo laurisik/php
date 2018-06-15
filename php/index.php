48ad1036c0b8aae9f0d1394fca7e1bd04d5749a3
Lauris <otraisk@gmail.com>
2018-06-15 10:49:26 +0300

    added php project

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.1">
+    <link href = "./libs/bootstrap/css/bootstrap.css" rel ="stylesheet">
+    <script src="./libs/jQuery/jquery2.1.4.min.js"></script>
+    <script src="./libs/bootstrap/js/bootstrap.js"> </script>
+    <link rel="stylesheet" href="./my.css"/>
+    </head>
+<body>
+	<h1> My first PHP website</h1>
+
+
+
+</body>
+</html>
